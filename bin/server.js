c3c09094a6f4d4cb229df4116c68fa0e551a9a40
Mihai Enescu <mihai.enescu@outlook.com>
2018-07-18 18:07:20 -0700

    Changed to make it a simple topic handler

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
+const _ = require('lodash');
 const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
 const router = express.Router();
-
 const mysql = require('mysql');
 const connectionPool = mysql.createPool({
     host     : process.env.DB_HOST,
@@ -18,45 +18,59 @@ const connectionPool = mysql.createPool({
  */
 let lastRevisionCount = 0;
 
-router.ws('/table', function(ws, req) {
+app.use('/topic/subscribe', router);
+router.ws('/lead', (ws, req) => {
 
-    ws.on('message', (msg) => {
+    let sendExternalMessage = (msg) => {
 
-        let query = `SELECT COUNT(ar.id) AS revision_count FROM audit_revisions ar JOIN audit_${msg} al ON al.rev = ar.id`;
+        if (1 === ws.readyState) {
+            ws.send(msg);
+        }
 
-        connectionPool.query(query, (error, results) => {
+        console.log(msg);
+    };
 
-            /**
-             * Response Object
-             * @type {{}}
-             */
-            let responseMessage = {
-                reload: false,
-                params: req.params
-            };
 
-            if (error) {
+    //todo: query every moment (last hour).
+    let timer = setInterval(_.debounce(() => {
+        connectionPool.query(
+            `
+                SELECT COUNT(ar.id) AS revision_count 
+                FROM audit_revisions ar JOIN audit_lead al ON al.rev = ar.id
+                WHERE ar.timestamp >= DATE_SUB(NOW(), INTERVAL 1 HOUR);
+            `,
+            (error, results) => {
 
-                responseMessage.error = true;
-                responseMessage.code = error.code;
-                responseMessage.message = error.sqlMessage;
+                let responseMessage = {
+                    reload: false,
+                    params: req.params
+                };
 
-                ws.send(JSON.stringify(responseMessage));
-                return;
+                if (error) {
+
+                    responseMessage['error'] = true;
+                    responseMessage['code'] = error.code;
+                    responseMessage['message'] = error.sqlMessage;
+
+                    sendExternalMessage(JSON.stringify(responseMessage));
+                    return;
+                }
+
+                if (results[0] && results[0]['revision_count'] !== lastRevisionCount) {
+                    lastRevisionCount = results[0]['revision_count'];
+                    responseMessage['reload'] = true;
+                    responseMessage['lastRevisionCount'] = lastRevisionCount;
+                }
+
+                sendExternalMessage(JSON.stringify(responseMessage));
             }
+        );
+    }, 500), 1500);
 
-            if (results[0] && results[0]['revision_count'] !== lastRevisionCount) {
-                lastRevisionCount = results[0]['revision_count'];
-                responseMessage.reload = true;
-                responseMessage.lastRevisionCount = lastRevisionCount;
-            }
-
-            ws.send(JSON.stringify(responseMessage));
-
-        });
-
+    ws.on('close', () => {
+        clearInterval(timer);
+        timer = null;
     });
 });
 
-app.use('/audit', router);
 app.listen(3000);
